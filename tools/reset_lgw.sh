f378bbe8b33903186539b258b688e6a520936515
bigbearishappy <953308023@qq.com>
2024-08-15 08:44:38 +0000

    update reset script
    Signed-off-by: bigbearishappy <953308023@qq.com>

--- tools/reset_lgw.sh
+++ tools/reset_lgw.sh
@@ -32,17 +32,6 @@ reset() {
     echo "CoreCell power enable through GPIO$SX1302_POWER_EN_PIN..."
     echo "CoreCell ADC reset through GPIO$AD5338R_RESET_PIN..."
 
-    # write output for SX1302 CoreCell power_enable and reset
-    #echo "1" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/value; WAIT_GPIO
-
-    #echo "1" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
-    #echo "0" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
-
-    #echo "0" > /sys/class/gpio/gpio$SX1261_RESET_PIN/value; WAIT_GPIO
-    #echo "1" > /sys/class/gpio/gpio$SX1261_RESET_PIN/value; WAIT_GPIO
-
-    #echo "0" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/value; WAIT_GPIO
-    #echo "1" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/value; WAIT_GPIO
     gpioset 2 7=1
     WAIT_GPIO
     gpioset 2 7=0
